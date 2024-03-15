4b00dfaa31933990ba1c3a68aa45082473dde75b
Tash <shitashni@gmail.com>
2024-03-15 20:58:46 +1300

    change 1

--- HandicraftStore/Controllers/ExchangeController.cs
+++ HandicraftStore/Controllers/ExchangeController.cs
@@ -14,7 +14,7 @@ namespace HandicraftStore.Controllers
     public class ExchangeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        //test commit
+        //test commit second commit
         public ExchangeController(ApplicationDbContext context)
         {
             _context = context;
